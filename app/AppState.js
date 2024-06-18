a600ffeef7450da3f0ad36effc792ea4798e3c73
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-18 15:19:40 -0600

    Inserting data into HTML, restyling card images

--- app/AppState.js
+++ app/AppState.js
@@ -11,49 +11,49 @@ class ObservableAppState extends EventEmitter {
   snacks = [
     
       new Snack (
-        'chips',
+        'Chips',
         '3.50',
         "https://images.unsplash.com/photo-1599490659213-e2b9527bd087?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       ),
 
       new Snack (
-        'candy',
+        'Candy',
         '2.50',
         'https://images.unsplash.com/photo-1621939514649-280e2ee25f60?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       ),
 
       new Snack (
-      'hummus',
+      'Hummus',
       '4.50',
       'https://images.unsplash.com/photo-1536974851270-95538a335a81?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       ),
 
       new Snack (
-      'healthySnacks',
+      'Healthy Snacks',
       '5.00',
       'https://images.unsplash.com/photo-1569420067112-b57b4f024595?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c25hY2tzfGVufDB8fDB8fHww'
       ),
 
     new Snack (
-      'charcuterieBoard',
+      'Charcuterie Board',
       '10.00',
       'https://images.unsplash.com/photo-1517093602195-b40af9688b46?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c25hY2tzfGVufDB8fDB8fHww'
     ),
 
     new Snack (
-      'donuts',
+      'Donuts',
       '4.50',
       'https://images.unsplash.com/photo-1575549595260-623d27ba5e44?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fHNuYWNrc3xlbnwwfHwwfHx8MA%3D%3D'
     ),
 
     new Snack (
-      'cookies',
+      'Cookies',
       '3.00',
       'https://images.unsplash.com/photo-1599629954294-14df9ec8bc05?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDJ8fHNuYWNrc3xlbnwwfHwwfHx8MA%3D%3D'
     ),
 
     new Snack (
-      'popcorn',
+      'Popcorn',
       '1.50',
       'https://images.unsplash.com/photo-1512149177596-f817c7ef5d4c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHNuYWNrc3xlbnwwfHwwfHx8MA%3D%3D'
     )
--- app/models/Snacks.js
+++ app/models/Snacks.js
@@ -9,10 +9,10 @@ export class Snack {
         return `
         <div class="col-12 col-md-3">
         <div class="card" style="width: 18rem;" role="button">
-          <img src="https://images.unsplash.com/photo-1621447504864-d8686e12698c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fHNuYWNrc3xlbnwwfHwwfHx8MA%3D%3D" class="card-img-top" alt="Test Image One">
+          <img src="${this.image}" class="card-img-top img-style" alt="Image of ${this.name}"">
           <div class="card-body">
-            <p class="card-text fw-bold text-center">Test One</p>
-            <p class="card-text">Price: $9.99</p>
+            <p class="card-text fw-bold text-center">${this.name}</p>
+            <p class="card-text">Price: $${this.price}</p>
           </div>
         </div>
       </div>
