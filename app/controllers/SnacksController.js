a0d49523bc2d4cd964c292f881e178d32918b833
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-18 15:06:47 -0600

    drawing to HTML dynamically

--- app/controllers/SnacksController.js
+++ app/controllers/SnacksController.js
@@ -1,4 +1,17 @@
+import { AppState } from "../AppState.js"
+
 export class SnacksController {
     constructor() {
         console.log('From the SnacksController')
-}}
+        this.drawSnacks()
+}
+    drawSnacks(){
+        const snacks = AppState.snacks
+        // ✅ console.log('Drawing the snacks', snacks)
+        let innerHTMLString = ''
+        snacks.forEach((snack) => innerHTMLString += snack.catalogHTMLTemplate)
+        const snacksCatalog = document.getElementById('snacksCatalog')
+        snacksCatalog.innerHTML = innerHTMLString
+    }
+
+}
